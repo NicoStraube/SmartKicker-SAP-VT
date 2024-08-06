77e203e94b3c2d437ed5900c76d9ff2ca5e5132f
NicoStraube <nicostraube04+git@gmail.com>
2024-08-06 10:09:52 +0200

    [misc] Restructure ApplicationWrapper project

--- ApplicationWrapper/build.gradle.kts
+++ ApplicationWrapper/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    alias(libs.plugins.shadow) // id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "dev.nstraube.sap.smartkicker"
+group = "com.sap.vt.smartkicker"
 version = "1.0-SNAPSHOT"
 
 repositories {
---
+++ ApplicationWrapper/src/main/kotlin/com/sap/vt/smartkicker/main.kt
@@ -0,0 +1,5 @@
+package com.sap.vt.smartkicker
+
+fun main() {
+    
+}
